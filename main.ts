860f8b1132f0c86eac1816b309ad39661f79eadd
Parjinder Pannu <ppannu@cisco.com>
2017-08-15 16:38:18 -0700

    Simple program for observables

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-alert("OK!!");
+import { Observable } from "rxjs";
+
+let numbers = [1, 5, 10];
+let source = Observable.from(numbers);
+
+class MyObserver {
+
+    next(value) {
+        console.log(`Value: ${value}`);
+    }
+
+    error(e) {
+        console.log(`error: ${e}`);
+    }
+
+    complete() {
+        console.log("complete");
+    }
+}
+
+source.subscribe(new MyObserver());
