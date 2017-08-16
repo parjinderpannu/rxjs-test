c11134373c0d257fdf0069ce102a4679321b6b88
Parjinder Pannu <ppannu@cisco.com>
2017-08-16 16:44:34 -0700

    Simple Observable with Observable.throw(new Error("Stop!"))

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
 import { Observable } from "rxjs";
 import { load, loadWithFetch } from "./loader";
 
-let source = Observable.create(observer => {
-    observer.next(1);
-    observer.next(2);
-    observer.error("Stop!");
-    observer.next(3);
-    observer.complete();
-});
+let source = Observable.merge(
+    Observable.of(1),
+    Observable.from([2,3,4]),
+    Observable.throw(new Error("Stop!")),
+    Observable.of(5)
+);
 
 source.subscribe(
     value => console.log(`value: ${value}`),
