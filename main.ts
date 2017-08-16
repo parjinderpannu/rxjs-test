ee2bb383c8a9facccdd4c4e1df2c66e7391d8315
Parjinder Pannu <ppannu@cisco.com>
2017-08-15 21:33:23 -0700

    small subscribe with =>

--- main.ts
+++ main.ts
@@ -3,19 +3,8 @@ import { Observable, Observer } from "rxjs";
 let numbers = [1, 5, 10];
 let source = Observable.from(numbers);
 
-class MyObserver implements Observer<number> {
-
-    next(value) {
-        console.log(`Value: ${value}`);
-    }
-
-    error(e) {
-        console.log(`error: ${e}`);
-    }
-
-    complete() {
-        console.log("complete");
-    }
-}
-
-source.subscribe(new MyObserver());
+source.subscribe(
+    value => console.log(`Value: ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
+);
