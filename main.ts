f41ffcd28094d1970ff0eed12c4dfb7e764402a8
Parjinder Pannu <ppannu@cisco.com>
2017-08-16 00:06:08 -0700

    Processing Mouse Events

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Observable } from "rxjs";
 
+let circle = document.getElementById("circle");
 let source = Observable.fromEvent(document, "mousemove")
                         .map((e : MouseEvent) => {
                             return {
@@ -7,10 +8,16 @@ let source = Observable.fromEvent(document, "mousemove")
                                 y: e.clientY
                             }
                         })
-                        .filter(value => value.x < 500);
+                        .filter(value => value.x < 500)
+                        .delay(300);
+                        
+function onNext(value) {
+    circle.style.left = value.x;
+    circle.style.top = value.y;
+}
 
 source.subscribe(
-    value => console.log(value),
+    onNext,
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
