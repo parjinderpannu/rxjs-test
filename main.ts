9427bd5b913dc98085ac00a25af2f225955855b2
Parjinder Pannu <ppannu@cisco.com>
2017-08-15 21:45:21 -0700

    Observable create function and for loop

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { Observable, Observer } from "rxjs";
 
 let numbers = [1, 5, 10];
-let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+
+    for(let n of numbers){
+        observer.next(n);
+    }
+
+    observer.complete();
+});
 
 source.subscribe(
     value => console.log(`Value: ${value}`),
