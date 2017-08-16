6e839f544f9bdfef8747c4a45207695dcc9c779e
Parjinder Pannu <ppannu@cisco.com>
2017-08-16 11:48:05 -0700

    fetch doesn't do lazy loading && uninstalled both whatwg-streams & fetch

--- main.ts
+++ main.ts
@@ -24,6 +24,10 @@ function load(url: string) {
     }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
 }
 
+function loadWithFetch(url: string){
+    return Observable.fromPromise(fetch(url).then(r => r.json()));
+}
+
 function retryStrategy({attempts = 4, delay = 1000}) {
     return function(errors) {
         return errors
@@ -44,7 +48,9 @@ function renderMovies(movies) {
     });
 }
 
-click.flatMap(e => load("movies.json"))
+loadWithFetch("movies.json");
+
+click.flatMap(e => loadWithFetch("movies.json"))
     .subscribe(
     renderMovies,
     e => console.log(`error: ${e}`),
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="globals/whatwg-fetch/index.d.ts" />
-/// <reference path="globals/whatwg-streams/index.d.ts" />
+
